b6e01d4d79357a5d16e4e91d6479fbb6049bca38
Mary <mary00zou@gmail.com>
2016-07-27 14:08:42 -0700

    78% accuracy
    - added some feature engineering
    - changed algorithm to random forest

--- script.py
+++ script.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import numpy as np
 import re
+import matplotlib.pyplot as plt
 from sklearn import cross_validation as cv
 from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier as rf
 
 train = pd.read_csv('train.csv')
 test = pd.read_csv('test.csv')
@@ -18,49 +20,83 @@ def get_title(name):
 	except:
 		return ''
 
-'''********** Converting non-numerical columns to numbers **********'''
 
-#Converts the Sex column 
-train.loc[train['Sex'] == 'female', "Sex"] = 1
-train.loc[train['Sex'] == 'male', "Sex"] = 0
-test.loc[test['Sex'] == 'female', "Sex"] = 1
-test.loc[test['Sex'] == 'male', "Sex"] = 0
-
-#Converts the Embarked column
-train.loc[train['Embarked'] == 'S', 'Embarked'] = 0
-train.loc[train['Embarked'] == 'C', 'Embarked'] = 1
-train.loc[train['Embarked'] == 'Q', 'Embarked'] = 2
-test.loc[test['Embarked'] == 'S', 'Embarked'] = 0
-test.loc[test['Embarked'] == 'C', 'Embarked'] = 1
-test.loc[test['Embarked'] == 'Q', 'Embarked'] = 2
+def agegroup(age):
+	'''creates age groups'''
+	agegp = [9, 18, 30, 40, 50, 60, 100]
+	for i in agegp:
+		if age <= i:
+			return agegp.index(i)
+			break
 
 
-'''********************* Filling missing Data *********************'''
+def noble(title):
+	'''separates into nobility and commoners by title'''
+	tage = [['Mlle.', 'Ms.', 'Mme', 'Miss.', 'Mr.', 'Mrs.', 'Dr.', 'Rev.'], ['Countess.', 'Jonkheer.','Capt.', 'Col.', 'Sir.', 'Lady.', 'Major.', 'Don.', 'Dona.']]
+	for i in tage:
+		if title in i:
+			return tage.index(i)
+			break
 
-#fills missing ages for males using male median age'''
-train.loc[train['Sex'] == 0, 'Age'] = train.loc[train['Sex'] == 0, 'Age'].fillna(train[train['Sex'] == 0]['Age'].median())
-test.loc[test['Sex'] == 0, 'Age'] = test.loc[test['Sex'] == 0, 'Age'].fillna(test[test['Sex'] == 0]['Age'].median())
 
-#fills missing ages for females using female median age'''
-train.loc[train['Sex'] == 1, 'Age'] = train.loc[train['Sex'] == 1, 'Age'].fillna(train[train['Sex'] == 1]['Age'].median())
-test.loc[test['Sex'] == 1, 'Age'] = test.loc[test['Sex'] == 1, 'Age'].fillna(test[test['Sex'] == 1]['Age'].median())
 
-#fills missing embarked locations using the most common: 0'''
-train["Embarked"] = train["Embarked"].fillna(0)
-test["Embarked"] = test["Embarked"].fillna(0)
+def feature_engineer(df):
+	#creates the titles column
+	df['Title'] = df['Name'].apply(get_title)
 
-#fills other potentially missing columns
-test['Fare'] = test['Fare'].fillna(test['Fare'].median())
-test['Pclass'] = test['Pclass'].fillna(test['Pclass'].median())
-test['SibSp'] = test['SibSp'].fillna(test['SibSp'].median())
-test['Parch'] = test['Parch'].fillna(test['Parch'].median())
+	#creates the family size column
+	df['FSize'] = df['Parch'] + df['SibSp'] + 1
+
+	#creates noble column
+	df['Noble'] = df['Title'].apply(noble)
+	df['Noble'] = df['Noble'].fillna(1)
+
+	#Converts the Sex column to numerical values
+	df.loc[df['Sex'] == 'female', "Sex"] = 1
+	df.loc[df['Sex'] == 'male', "Sex"] = 0
+	
+	#Converts the Embarked column to numerical values
+	df.loc[df['Embarked'] == 'S', 'Embarked'] = 0
+	df.loc[df['Embarked'] == 'C', 'Embarked'] = 1
+	df.loc[df['Embarked'] == 'Q', 'Embarked'] = 2
+	
+	#fills missing ages for males using male median age'''
+	df.loc[df['Sex'] == 0, 'Age'] = df.loc[df['Sex'] == 0, 'Age'].fillna(df[df['Sex'] == 0]['Age'].median())
+
+	#fills missing ages for females using female median age'''
+	df.loc[df['Sex'] == 1, 'Age'] = df.loc[df['Sex'] == 1, 'Age'].fillna(df[df['Sex'] == 1]['Age'].median())
+
+	#fills missing embarked locations using the most common: 0'''
+	df["Embarked"] = df["Embarked"].fillna(0)
+
+	#fills other potentially missing columns
+	df['Fare'] = df['Fare'].fillna(df['Fare'].median())
+	df['Pclass'] = df['Pclass'].fillna(df['Pclass'].median())
+	df['SibSp'] = df['SibSp'].fillna(df['SibSp'].median())
+	df['Parch'] = df['Parch'].fillna(df['Parch'].median())
+
+	#creates age group column
+	df['AgeGp'] = df['Age'].apply(agegroup)
+	
+	#Converts Title column to numerical values
+	tls = df['Title'].unique().tolist()
+	for i in tls:
+		df.loc[df['Title'] == i, 'Title'] = tls.index(i)
+
+	return df
+
+
+train = feature_engineer(train)
+test = feature_engineer(test)
 
 
 '''************************** Prediction **************************''' 
-
+predictors = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked", 'FSize', 'AgeGp', 'Noble', 'Title']
 
 alg = LogisticRegression(random_state = 1)
-predictors = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked"]
+#alg = rf(random_state = 1, n_estimators = 1555, min_samples_split = 4, min_samples_leaf = 3)
+scores = cv.cross_val_score(alg, train[predictors], train['Survived'])
+print(np.mean(scores))
 
 alg.fit(train[predictors], train['Survived'])
 
@@ -72,3 +108,4 @@ submission = pd.DataFrame({
 	})
 
 submission.to_csv('kaggle.csv', index = False)
+
